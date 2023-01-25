82e13ef75f0e883e7d8eb07568606deb2a9af166
Amirmahdi Ansaripour <ansaripouramirmahdi@gmail.com>
2023-01-25 08:16:49 -0500

    modified topology

--- CA2/wireless.tcl
+++ CA2/wireless.tcl
@@ -73,14 +73,12 @@ $ns node-config -adhocRouting $val(rp) \
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
-set n3 [$ns node]
 #balancer
-set n4 [$ns node]	
+set n3 [$ns node]	
 #balancer
+set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
-set n7 [$ns node]
-set n8 [$ns node]
 
 # Nodes random motion
 $n0 random-motion 0
@@ -89,6 +87,7 @@ $n2 random-motion 0
 $n3 random-motion 0
 $n4 random-motion 0
 $n5 random-motion 0
+$n6 random-motion 0
 
 # Nodes size
 $ns initial_node_pos $n0 20
@@ -98,8 +97,6 @@ $ns initial_node_pos $n3 20
 $ns initial_node_pos $n4 20
 $ns initial_node_pos $n5 20
 $ns initial_node_pos $n6 20
-$ns initial_node_pos $n7 20
-$ns initial_node_pos $n8 20
 
 #Nodes locations
 $n0 set X_ 50.0
@@ -123,91 +120,77 @@ $n5 set Z_ 0.0
 $n6 set X_ 150.0
 $n6 set Y_ 180.0
 $n6 set Z_ 0.0
-$n7 set X_ 200.0
-$n7 set Y_ 280.0
-$n7 set Z_ 0.0
-$n8 set X_ 200.0
-$n8 set Y_ 180.0
-$n8 set Z_ 0.0
 
 #TCP connections
-set tcp0_4 [new Agent/TCP]
-set tcp1_4 [new Agent/TCP]
-set tcp2_4 [new Agent/TCP]
-set tcp3_4 [new Agent/TCP]
+set tcp0_3 [new Agent/TCP]
+set tcp1_3 [new Agent/TCP]
+set tcp2_3 [new Agent/TCP]
+set tcp3_3 [new Agent/TCP]
 
-set sink0_4 [new Agent/TCPSink]
-set sink1_4 [new Agent/TCPSink]
-set sink2_4 [new Agent/TCPSink]
-set sink3_4 [new Agent/TCPSink]
-$tcp0_4 set packetSize_ $packet_size.Kb
-$tcp1_4 set packetSize_ $packet_size.Kb
-$tcp2_4 set packetSize_ $packet_size.Kb
-$tcp3_4 set packetSize_ $packet_size.Kb
-$ns attach-agent $n0 $tcp0_4
-$ns attach-agent $n1 $tcp1_4
-$ns attach-agent $n2 $tcp2_4
-$ns attach-agent $n3 $tcp3_4
-$ns attach-agent $n4 $sink0_4
-$ns attach-agent $n4 $sink1_4
-$ns attach-agent $n4 $sink2_4
-$ns attach-agent $n4 $sink3_4
-$ns connect $tcp0_4 $sink0_4
-$ns connect $tcp1_4 $sink1_4
-$ns connect $tcp2_4 $sink2_4
-$ns connect $tcp3_4 $sink3_4
+set sink0_3 [new Agent/TCPSink]
+set sink1_3 [new Agent/TCPSink]
+set sink2_3 [new Agent/TCPSink]
 
-set cbr0_4 [new Application/Traffic/CBR]
-set cbr1_4 [new Application/Traffic/CBR]
-set cbr2_4 [new Application/Traffic/CBR]
-set cbr3_4 [new Application/Traffic/CBR]
+$tcp0_3 set packetSize_ $packet_size.Kb
+$tcp1_3 set packetSize_ $packet_size.Kb
+$tcp2_3 set packetSize_ $packet_size.Kb
 
-$cbr0_4 attach-agent $tcp0_4
-$cbr1_4 attach-agent $tcp1_4
-$cbr2_4 attach-agent $tcp2_4
-$cbr3_4 attach-agent $tcp3_4
+$ns attach-agent $n0 $tcp0_3
+$ns attach-agent $n1 $tcp1_3
+$ns attach-agent $n2 $tcp2_3
+
+$ns attach-agent $n3 $sink0_3
+$ns attach-agent $n3 $sink1_3
+$ns attach-agent $n3 $sink2_3
+
+$ns connect $tcp0_3 $sink0_3
+$ns connect $tcp1_3 $sink1_3
+$ns connect $tcp2_3 $sink2_3
+
+set cbr0_3 [new Application/Traffic/CBR]
+set cbr1_3 [new Application/Traffic/CBR]
+set cbr2_3 [new Application/Traffic/CBR]
+set cbr3_3 [new Application/Traffic/CBR]
+
+$cbr0_3 attach-agent $tcp0_3
+$cbr1_3 attach-agent $tcp1_3
+$cbr2_3 attach-agent $tcp2_3
 
 $ns at 0.0 "initNetwork"
-$ns at 0.0 "$cbr0_4 start"
-$ns at 0.0 "$cbr1_4 start"
-$ns at 0.0 "$cbr2_4 start"
-$ns at 0.0 "$cbr3_4 start"
+$ns at 0.0 "$cbr0_3 start"
+$ns at 0.0 "$cbr1_3 start"
+$ns at 0.0 "$cbr2_3 start"
 
 set time 1.0
 
 proc initNetwork {} {
-    global sink0_4 sink1_4 sink2_4 sink3_4 ns time
+    global sink0_3 sink1_3 sink2_3 ns time
     #Initialize sinks
-    $sink0_4 set bytes_ 0
-    $sink1_4 set bytes_ 0
-    $sink2_4 set bytes_ 0
-    $sink3_4 set bytes_ 0
-
-
-
+    $sink0_3 set bytes_ 0
+    $sink1_3 set bytes_ 0
+    $sink2_3 set bytes_ 0
+    
     set currTime [$ns now]
     $ns at [expr $currTime+$time] "handleSink"
 }
 
 proc handleSink {} {
-    global sink0_4 sink1_4 sink2_4 sink3_4
+    global sink0_3 sink1_3 sink2_3
     global throughput5 time ns packet_size
     # global throughput5 throughput6 throughput7 throughput8 time ns packet_size
 
-    set link0_4 [$sink0_4 set bytes_]
-    set link1_4 [$sink1_4 set bytes_]
-    set link2_4 [$sink2_4 set bytes_]
-    set link3_4 [$sink3_4 set bytes_]
-
+    set link0_3 [$sink0_3 set bytes_]
+    set link1_3 [$sink1_3 set bytes_]
+    set link2_3 [$sink2_3 set bytes_]
+    
     set currTime [$ns now]
     # The load on sink
-    set load4 [expr ($link0_4*8)/$time + ($link1_4*8)/$time + ($link2_4*8)/$time + ($link3_4*8)/$time]
+    set load3 [expr ($link0_3*8)/$time + ($link1_3*8)/$time + ($link2_3*8)/$time]
 
-    puts $throughput5 "$currTime [expr $load4]"
-    $sink0_4 set bytes_ 0
-    $sink1_4 set bytes_ 0
-    $sink2_4 set bytes_ 0
-    $sink3_4 set bytes_ 0
+    puts $throughput5 "$currTime [expr $load3]"
+    $sink0_3 set bytes_ 0
+    $sink1_3 set bytes_ 0
+    $sink2_3 set bytes_ 0
     # Repeat the process till time = 100
     $ns at [expr $currTime+$time] "handleSink"
 }

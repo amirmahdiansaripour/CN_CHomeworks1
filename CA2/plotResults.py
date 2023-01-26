9313495d01abdec0b135929bee2175406f2a4c5b
Amirmahdi Ansaripour <ansaripouramirmahdi@gmail.com>
2023-01-26 03:48:11 -0500

    delay-BW works

--- CA2/plotResults.py
+++ CA2/plotResults.py
@@ -3,19 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from subprocess import DEVNULL, STDOUT, check_call
 
-def plot(xLabel, yLabel):
+def plot(Xs, Ys, title_, x_label, y_label):
     plt.figure()
 
     # # xLabel = list(range(1, 100))
-    # through4 = np.genfromtxt("throughput4.tr", dtype=np.int32)
-    # through5 = np.genfromtxt("throughput5.tr", dtype=np.int32)
-    # through6 = np.genfromtxt("throughput6.tr", dtype=np.int32)
     
     # yLabel = through4[:, 1]
     # xLabel = through4[:, 0]
-    plt.plot(xLabel, yLabel)
-    # plt.plot(xLabel, through5[:, 1])
-    # plt.plot(xLabel, through6[:, 1])
+    plt.plot(Xs, Ys, marker='o')
+    plt.title(title_)
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
     
     plt.show()
 
@@ -23,14 +21,14 @@ def plot(xLabel, yLabel):
 def extractData():
     openTRFile = open("wireless.tr", "r")
     data = []
-    packetDict = {}
-    delayDict = {}
+    sentTimes = {}
+    arrivalTimes = {}
     for row in openTRFile:
         data.append(str(row))
     for pattern in row:
         if re.search('^[dsr].*-It tcp.*', pattern):
             data.append(pattern)
-    print("Data Len: " , len(data))
+    # print("Data Len: " , len(data))
     for line in data:
         resultMatched = re.search('^([srd]) -t ([0-9]*[.]?[0-9]*) .*-Hs (\d+) .*-Ii (\d+) .*', line)
         if not resultMatched: continue
@@ -39,37 +37,40 @@ def extractData():
         time = resultMatched.group(2)
         hs = resultMatched.group(3)
         uid = resultMatched.group(4)
-        
-        if status == 's':
-            if re.search('[012]', hs):  # sender should be 0 1 2
-                if uid not in packetDict:
-                    packetDict[uid] = time  # packet is sent now!
-            
-        elif status == 'r':
-            if uid in packetDict:
-                print(status + " " + time + " " + hs + " " + uid)
-                if re.search('[456]', hs):
-                    delayDict[uid] = (float(time) - float(packetDict[uid]))
 
-        elif status == 'd': # if packet is delayed
-            if uid in packetDict:
-                packetDict.pop(uid)                
-                if uid in delayDict:
-                    delayDict.pop(uid) 
-    return sum(delayDict.values()) / len(delayDict)
+        if status == 's':
+            if re.search('[3]', hs):  # sender should be 3
+                if uid not in sentTimes:
+                    sentTimes[uid] = time  # packet is sent now!
+
+        elif status == 'r':
+            if uid in sentTimes:
+                # print(status + " " + time + " " + hs + " " + uid)
+                if re.search('[456]', hs): # receivers should be 4 5 6
+                    arrivalTimes[uid] = (float(time) - float(sentTimes[uid]))
+
+        elif status == 'd': # if should be retransmitted (TIMEOUT)
+            if uid in sentTimes:
+                sentTimes.pop(uid)                
+                if uid in arrivalTimes:
+                    arrivalTimes.pop(uid) 
+    return sum(arrivalTimes.values()) / len(arrivalTimes)
 
 
 def runMultipleTimes():
     diffBandWidths = [1, 10, 100]
     errorRates = [0.001, 0.0001, 0.00001]
-    points = []
+    Xs = []
+    Ys = []
     for bw in diffBandWidths:
         for errorRate in errorRates:
+            print("Running for BW: " + str(bw) + "Mbps and ErrorRate: " + str(errorRate))
             check_call(['ns', "wireless.tcl", str(errorRate), str(bw)], stdout=DEVNULL,
                        stderr=STDOUT)
             OneWayDelay = extractData()
-            points.append([errorRate, OneWayDelay])
-    plot(points[:, 0], points[:, 1])
+            Xs.append(bw)
+            Ys.append(OneWayDelay)
+    plot(Xs, Ys, "OneWatyDelay vs Bandwidth", "BandWidth (Mbps)", "OneWayDelay (s)")
 # AverageDelay =  extractData()
 
 runMultipleTimes()
